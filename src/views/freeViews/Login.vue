b0b1b8e2b0e1737e6b726598f77613e49c387f73
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-12 15:58:59 -0500

    implement login view

--- src/views/freeViews/Login.vue
+++ src/views/freeViews/Login.vue
@@ -1,11 +1,43 @@
 <template>
   <v-container>
-    Iniciar sesión
+    <v-form class="col-12 col-sm-8 col-md-5 mx-auto flex">
+      <div class="icon mx-auto mb-5 img-container">
+        <img src="../../assets/calculator-icon.png">
+      </div>
 
-    <v-form>
-      <v-btn to="auth">
+      <h2 class="text-center mb-10">Iniciar sesión</h2>
+
+      <v-text-field
+          label="Correo electrónico"
+          color="accent"
+          outlined
+      ></v-text-field>
+
+      <v-text-field
+          label="Contraseña"
+          color="accent"
+          outlined
+      ></v-text-field>
+
+      <v-btn
+          to="auth"
+          color="secondary"
+          class="py-5 mb-5"
+          block
+      >
         Iniciar sesión
       </v-btn>
+
+      <p class="text-center">
+        ¿No tienes cuenta?
+        <v-btn
+            text
+            class="blue--text"
+            to="/register"
+        >
+          Registrate
+        </v-btn>
+      </p>
     </v-form>
   </v-container>
 </template>
@@ -15,4 +47,25 @@
 export default {
   name: "Login"
 }
-</script> 
+</script>
+
+<style scoped>
+  .icon {
+    width: 100px;
+    height: 100px;
+    background: #A5EBE9;
+    border-radius: 50%;
+  }
+
+  .img-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img {
+    max-width: 70px;
+    margin: auto;
+    display: block;
+  }
+</style>
