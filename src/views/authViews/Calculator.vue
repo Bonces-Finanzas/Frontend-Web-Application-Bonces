528876caab2a370a0e8d1c729ab2682902c74725
Jesús <jesuspacheco0219@gmail.com>
2022-06-14 19:48:38 -0500

    Mockup Datos del Bono
    Maquetando Datos del bono

--- src/views/authViews/Calculator.vue
+++ src/views/authViews/Calculator.vue
@@ -1,12 +1,237 @@
 <template>
-  <v-container>
-    Calculadora
-  </v-container>
+  <v-form>
+    <v-container fluid>
+      <h1 class="text-left mb-10">Ingreso de Datos</h1>
+      <v-row>
+        <v-col class="col-12 col-sm-8 col-md-5 mx-auto flex">
+          <h2 class="text-center mb-10">Datos del Bono</h2>
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Valor Nominal</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+
+                  prefix="s/"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Valor Comercial</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+                  prefix="s/"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Numero de años</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Frecuencia del cupón</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-select
+                  v-model="form.couponFrequency"
+                  :hint="`${form.couponFrequency.state}, ${form.couponFrequency.abbr}`"
+                  :items="form.couponFrequencyItems"
+                  item-text="state"
+                  item-value="abbr"
+                  persistent-hint
+                  return-object
+                  single-line
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Frecuencia del cupón</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-select
+                  v-model="form.interestRateType"
+                  :hint="`${form.interestRateType.state}, ${form.interestRateType.abbr}`"
+                  :items="form.interestRateTypeItems"
+                  item-text="state"
+                  item-value="abbr"
+                  persistent-hint
+                  return-object
+                  single-line
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Capitalización</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-select
+                  v-model="form.capitalization"
+                  :hint="`${form.capitalization.state}, ${form.capitalization.abbr}`"
+                  :items="form.capitalizationItems"
+                  item-text="state"
+                  item-value="abbr"
+                  persistent-hint
+                  return-object
+                  single-line
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Tasa de interés</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+
+                  suffix="%"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Tasa anual de descuento</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+
+                  suffix="%"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Impuesto a la Renta</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+
+                  suffix="%"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="4">
+              <v-subheader class="font-weight-medium">Fecha de emisión</v-subheader>
+            </v-col>
+            <v-col cols="8">
+              <v-menu
+                  ref="menu"
+                  v-model="form.issueDateMenu"
+                  :close-on-content-click="false"
+                  :return-value.sync="form.issueDate"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                      v-model="form.issueDate"
+                      label="Click aquí"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                    v-model="form.issueDate"
+                    no-title
+                    scrollable
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn
+                      text
+                      color="primary"
+                      @click="form.issueDateMenu = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                      text
+                      color="primary"
+                      @click="$refs.menu.save(form.issueDate)"
+                  >
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
+
+            </v-col>
+          </v-row>
+
+        </v-col >
+        <v-col class="col-12 col-sm-8 col-md-5 mx-auto flex">
+          <h2 class="text-center mb-10">Datos de los costes</h2>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 
 export default {
-  name: "Calculator"
+  name: "Calculator",
+  data () {
+    return {
+      form: {
+        nominalValue:0,
+        valorCommercial:0,
+        numberOfYears:0,
+        couponFrequency: {state: '', abbr: ''},
+        couponFrequencyItems: [
+          {state: 'Diaria', abbr: '1 día'},
+          {state: 'Mensual', abbr: '30 días'},
+          {state: 'Bimestral', abbr: '60 días'},
+          {state: 'Trimestral', abbr: '90 días'},
+          {state: 'Semestral', abbr: '180 días'},
+          {state: 'Anual', abbr: '360 días'},
+        ],
+
+        daysforYears:0,
+
+        interestRateType:{state: '', abbr:''},
+        interestRateTypeItems: [
+          {state: 'Nominal', abbr: 'n'},
+          {state: 'Efectiva', abbr: 'e'},
+        ],
+
+        capitalization:{state: '', abbr:''},
+        capitalizationItems: [
+          {state: 'Diaria', abbr: '1 días'},
+          {state: 'Mensual', abbr: '30 días'},
+          {state: 'Bimestral', abbr: '60 días'},
+          {state: 'Trimestral', abbr: '90 días'},
+          {state: 'Semestral', abbr: '180 días'},
+          {state: 'Anual', abbr: '360 días'},
+        ],
+        interestRate:0,
+        annualDiscountRate:0,
+        incomeTax:0,
+        issueDate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        issueDateMenu: false,
+
+      }
+    }
+  },
 }
-</script> 
+</script>
