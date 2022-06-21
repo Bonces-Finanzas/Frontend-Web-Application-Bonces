2a7b3589a75c8d5e8b3bd4704c0eeaa8c77d1c86
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-21 17:18:40 -0500

    finished myProfile view

--- src/views/authViews/MyProfile.vue
+++ src/views/authViews/MyProfile.vue
@@ -1,7 +1,11 @@
 <template>
   <v-container>
     <h1 class="text-center mb-16">Mi perfil</h1>
-    <v-form class="my-auto flex">
+    <v-form
+        class="my-auto flex"
+        :readonly="!isEdit"
+        :disabled="!isEdit"
+    >
       <v-row>
         <v-col class="col-12 col-sm-10 col-lg-6 mx-auto">
           <v-row>
@@ -9,7 +13,10 @@
               <v-subheader class="font-weight-medium">Nombre:</v-subheader>
             </v-col>
             <v-col class="col-8">
-              <v-text-field solo></v-text-field>
+              <v-text-field
+                  v-model="user.name"
+                  solo
+              ></v-text-field>
             </v-col>
           </v-row>
           <v-row>
@@ -17,7 +24,10 @@
               <v-subheader class="font-weight-medium">Apellido:</v-subheader>
             </v-col>
             <v-col class="col-8">
-              <v-text-field solo></v-text-field>
+              <v-text-field
+                  v-model="user.lastName"
+                  solo
+              ></v-text-field>
             </v-col>
           </v-row>
           <v-row>
@@ -25,25 +35,131 @@
               <v-subheader class="font-weight-medium">Correo:</v-subheader>
             </v-col>
             <v-col class="col-8">
-              <v-text-field solo></v-text-field>
+              <v-text-field
+                  v-model="user.email"
+                  solo
+              ></v-text-field>
             </v-col>
           </v-row>
         </v-col>
       </v-row>
 
+      <v-row
+          v-if="isEdit"
+          class="justify-center mt-10"
+      >
+        <v-btn
+            v-if="!authStore.loading"
+            class="mr-3"
+            width="150px"
+            color="secondary"
+            @click="saveProfileData"
+        >
+          Guardar
+        </v-btn>
+        <v-btn
+            v-if="!authStore.loading"
+            class="ml-3"
+            width="150px"
+            @click="cancelEditProfile"
+        >
+          Cancelar
+        </v-btn>
+        <div
+            v-else
+            class="flex d-flex"
+        >
+          <v-progress-circular
+              indeterminate
+              color="primary"
+              class="mx-auto mb-5"
+          ></v-progress-circular>
+        </div>
+      </v-row>
       <v-btn
+          v-else
           class="center mt-10"
           width="150px"
           color="secondary"
-      >Editar</v-btn>
+          @click="() => { this.isEdit = true; }"
+      >
+        Editar
+      </v-btn>
     </v-form>
+
+    <v-snackbar
+        v-model="authStore.error"
+        :timeout="timeout"
+        color="red accent-2"
+    >
+      {{ "Error: hubo un error al actualizar los datos" }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="accent"
+            text
+            v-bind="attrs"
+            @click="authStore.error = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <v-snackbar
+        v-model="successfulUpdate"
+        :timeout="timeout"
+        color="primary"
+        content-class="accent--text"
+    >
+      {{ "Actualización de datos existosa" }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="accent"
+            text
+            v-bind="attrs"
+            @click="successfulUpdate = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-container>
 </template>
 
 <script>
+import { useAuthStore } from "@/store/useAuthStore";
 
 export default {
-  name: "MyProfile"
+  name: "MyProfile",
+  data() {
+    return {
+      authStore: useAuthStore(),
+      user: {},
+      isEdit: false,
+      timeout: 3000,
+      successfulUpdate: false,
+    }
+  },
+  methods: {
+    async saveProfileData() {
+      const { name, lastName, email } = this.user;
+      await this.authStore.update(this.user.id, { name, lastName, email });
+      if (this.authStore.error === false)
+        this.successfulUpdate = true;
+    },
+    retrieveUser() {
+      this.user = Object.assign({}, this.authStore.user);
+    },
+    cancelEditProfile() {
+      this.isEdit = false;
+      this.retrieveUser();
+    }
+  },
+  mounted() {
+    this.retrieveUser();
+  }
 }
 </script>
 
