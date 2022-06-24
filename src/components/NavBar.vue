4548b5491992d5283cbdd92301325294e8287b42
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-24 12:33:34 -0500

    responsive problems fixed in navbar and footer

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,11 +12,42 @@
         :to="item.path"
         @click="item.action"
         exact
-        class="primary--text"
+        class="primary--text d-none d-sm-flex"
         text
     >
       {{ item.name }}
     </v-btn>
+
+    <v-app-bar-nav-icon
+        class="accent primary--text d-flex d-sm-none"
+        @click.stop="drawer = !drawer"
+    ></v-app-bar-nav-icon>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+        absolute
+        temporary
+        right
+        color="accent"
+    >
+      <v-list
+          v-for="item in items"
+          :key="item.name"
+          nav
+          dense
+      >
+        <v-btn
+            :to="item.path"
+            @click="item.action"
+            exact
+            class="primary--text"
+            text
+        >
+          {{ item.name }}
+        </v-btn>
+      </v-list>
+    </v-navigation-drawer>
   </v-app-bar>
 </template>
 
@@ -25,6 +56,17 @@ export default {
   name: "NavBar",
   props: {
     items: Array
+  },
+  data() {
+    return {
+      drawer: false,
+      group: null,
+    }
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    }
   }
 }
 </script>
